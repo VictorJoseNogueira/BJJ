2a1d172b87b23e0c4022de75c24081223fab482c
Victor Nogueira <victorjose.nogueirasantos@hotmail.com>
2024-06-20 05:40:10 -0300

    test_package
    adicionando um pacote de testes

--- handbook/urls.py
+++ handbook/urls.py
@@ -1,5 +1,3 @@
-
-from django.contrib import admin
 from django.urls import path
 from handbook import views
 
@@ -9,7 +7,6 @@ app_name = 'moves'
 urlpatterns = [
     path('', views.home, name='home'),
     path('moves/<int:movement_id>/', views.move, name="move"),
-    path('moves/dificuldades/<int:difficulty_id>/', views.difficulty_def, name="difficulty"),
-    path('moves/categorias/<int:category_id>/', views.category_def, name="category"),
+    path('moves/dificuldades/<int:difficulty_id>/', views.difficulty_def, name="difficulty"),  # noqa:E501
+    path('moves/categorias/<int:category_id>/', views.category_def, name="category"),  # noqa:E501
 ]
- 
--- project/urls.py
+++ project/urls.py
@@ -20,5 +20,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',include('handbook.urls')), # dominio.com/
+    path('', include('handbook.urls')),  # dominio.com/
 ]
