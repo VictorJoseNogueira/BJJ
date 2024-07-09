d085ce65032e750591035a55921439f2668ade2e
Victor Nogueira <victorjose.nogueirasantos@hotmail.com>
2024-07-09 16:05:54 -0300

    .

---
+++ handbook/tests/test_move_base.py
@@ -0,0 +1,38 @@
+from django.test import TestCase
+from handbook.models import Category, Difficulty, Moves
+from django.contrib.auth.models import User
+
+
+class MovesTestBase(TestCase):
+    def setUp(self) -> None:
+        return super().setUp()
+
+    def make_category(self, category=''):
+        return Category.objects.create(category=category)
+
+    def make_author(self, first_name='user', last_name='name', username='username', password='321', email='username@email.com'):  # noqa E501
+        return User.objects.create_user(
+            first_name=first_name,
+            last_name=last_name,
+            username=username,
+            password=password,
+            email=email)
+
+    def make_difficulty(self, difficulty='DIFICULDADE'):
+        return Difficulty.objects.create(difficulty=difficulty)
+
+    def make_moviment(self, category_data=None, difficulty_data=None, author_data=None, title='Movement title', title_slug='movement-title', is_published=True):  # noqa E501  
+        if category_data is None:
+            category_data = {}
+        if difficulty_data is None:
+            difficulty_data = {}
+        if author_data is None:
+            author_data = {}
+
+        return Moves.objects.create(
+            category=self.make_category(**category_data),
+            difficulty=self.make_difficulty(**difficulty_data),
+            author=self.make_author(**author_data),
+            title=title,
+            title_slug=title_slug,
+            is_published=is_published)  # noqa E501
--- handbook/tests/test_moves_views.py
+++ handbook/tests/test_moves_views.py
@@ -1,10 +1,12 @@
-from django.test import TestCase
 from django.urls import reverse, resolve
 from handbook import views
+from .test_move_base import MovesTestBase
+
+# teste @skip("texto")
+# self.fail('texto')
 
 
-class Moves_views_test(TestCase):
-
+class Moves_views_test(MovesTestBase):
     # home
     def test_move_home_view_return_status_code_200_ok(self):
         response = self.client.get(reverse("moves:home"))
@@ -22,45 +24,72 @@ class Moves_views_test(TestCase):
         view = resolve(reverse("moves:home"))
         self.assertIs(view.func, views.home)
 
+    def test_moves_home_template_load_movements(self):
+        self.make_moviment()
+        response = self.client.get(reverse("moves:home"))
+        content = response.content.decode('utf-8')
+        response_context_movement = response.context['moves']
+        self.assertIn('DIFICULDADE', content)
+        self.assertNotIn('username', content)
+        self.assertEqual(len(response_context_movement), 1)
+
     # category
     def test_move_category_view_funcion_is_correct(self):
-        view = resolve(reverse("moves:category", kwargs={'category_id': 1}))
+        view = resolve(reverse("moves:category", kwargs={'category_id': 1000}))
         self.assertIs(view.func, views.category_def)
+
+    def test_move_category_view_return_404_if_no_moves_found(self):
+        response = self.client.get(reverse("moves:category", kwargs={'category_id': 1000}))  # noqa E501
+        self.assertEqual(response.status_code, 404)
+
+    def test_moves_category_template_loads_movements(self):
+        needed_tittle = "this is a category test"
+        # create a move
+        self.make_moviment(title=needed_tittle)
+
+        response = self.client.get(reverse("moves:category", kwargs={'category_id': 1},))  # noqa E501
+        content = response.content.decode("utf-8")
+        response_context_movements = response.context['moves']  # noqa
+        # check if one move exist
+        self.assertIn(needed_tittle, content)
+        ...
 
     # difficulty
     def test_move_difficulty_view_funcion_is_correct(self):
-        view = resolve(reverse("moves:difficulty", kwargs={'difficulty_id': 2}))  # noqa R501
+        view = resolve(reverse("moves:difficulty", kwargs={'difficulty_id': 2}))  # noqa E501
         self.assertIs(view.func, views.difficulty_def)
 
-# details
+    def test_move_difficulty_view_return_404_if_no_moves_found(self):
+        response = self.client.get(reverse("moves:difficulty", kwargs={'difficulty_id': 1000}))  # noqa E501
+        self.assertEqual(response.status_code, 404)
 
+    def test_moves_difficulty_template_loads_movements(self):
+        needed_tittle = "this is a difficulty test"
+        # create a move
+        self.make_moviment(title=needed_tittle)
+
+        response = self.client.get(reverse("moves:difficulty", kwargs={'difficulty_id': 1},))  # noqa E501
+        content = response.content.decode("utf-8")
+        response_context_movements = response.context['moves']  # noqa
+        # check if one move exist
+        self.assertIn(needed_tittle, content)
+
+    # details
     def test_move_details_view_funcion_is_correct(self):
         view = resolve(reverse("moves:move", kwargs={'movement_id': 2}))
         self.assertIs(view.func, views.move)
 
+    def test_move_details_view_return_404_if_no_moves_found(self):
+        response = self.client.get(reverse("moves:move", kwargs={'movement_id': 1000}))  # noqa E501
+        self.assertEqual(response.status_code, 404)
 
+    def test_moves_detail_template_loads_the_correct_movement(self):
+        needed_tittle = "this is a detail page - it load one move"
 
-'''    def test_move_category_view_return_status_code_200_ok(self):
-        response = self.client.get(reverse("moves:category"))
-        self.assertEqual(response.status_code, 200)
+        # create a move
+        self.make_moviment(title=needed_tittle)
 
-    def test_move_category_view_loads_correct_template(self):
-        response = self.client.get(reverse("moves:category"))
-        self.assertTemplateUsed(response, "handbook/pages/category.html")
-
-    def test_move_difficulty_view_return_status_code_200_ok(self):
-        response = self.client.get(reverse("moves:difficulty"))
-        self.assertEqual(response.status_code, 200)
-
-    def test_move_difficulty_view_loads_correct_template(self):
-        response = self.client.get(reverse("moves:difficulty"))
-        self.assertTemplateUsed(response, "handbook/pages/index.html")
-
-    def test_move_details_view_return_status_code_200_ok(self):
-        response = self.client.get(reverse("moves:move"))
-        self.assertEqual(response.status_code, 200)
-
-    def test_move_details_view_loads_correct_template(self):
-        response = self.client.get(reverse("moves:move"))
-        self.assertTemplateUsed(response, "handbook/pages/index.html")
-''' # noqa E501
+        response = self.client.get(reverse("moves:move", kwargs={'movement_id': 1},))  # noqa E501
+        content = response.content.decode("utf-8")
+        # check if one move exist
+        self.assertIn(needed_tittle, content)
